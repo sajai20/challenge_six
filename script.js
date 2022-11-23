61fc06571907c9e1bbb50aae82ba1cecc4c4e386
sajai20 <110714149+sajai20@users.noreply.github.com>
2022-11-23 18:57:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const btnEl = document.getElementById('btn');
+
+btnEl.addEventListener('click', () => {
+    btn.style.display = 'none';
+})
+
+clear.addEventListener('click', () => {
+    btn.style.display = 'block';
+})
